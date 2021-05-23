2f839c5d0c036dd4dfdb140b5302fe2738f5cb00
michael sipayung <michaelsipayung123@gmail.com>
2021-05-23 17:43:19 +0700

    Add project files.

---
+++ Dynamic Memory/Dynamic Memory.cpp
@@ -0,0 +1,35 @@
+﻿// Dynamic Memory.cpp : Defines the entry point for the application.
+//
+
+#include "Dynamic Memory.h"
+
+
+int main()
+{
+	std::cout << "Hello Dynamic Memory" << std::endl;
+	std::shared_ptr<std::string> p1; //point at a string 
+	std::shared_ptr<std::list<int>> p2;
+	std::cout << "a default initialized smart pointer holds a null pointer" << std::endl;
+	if (p1 && p2->empty()) {
+		*p1 = "hi";
+	}
+	std::shared_ptr<int> p3 = std::make_shared<int>(42);
+	std::shared_ptr<std::string> p4= std::make_shared<std::string>(19, 'a');
+	std::cout << "using auto to make the variable points to a dynamicially allocated" << std::endl;
+	auto p6 = std::make_shared<std::vector<std::string>>; //value initialized .. current value is empty string 
+	std::cout << "Copying shared ptr " << std::endl;
+	auto copy = std::make_shared<int>(87);
+	auto dest(copy);
+	auto r = std::make_shared<int>(42);
+	std::cout << "1.See the address \t:[" << dest << "]" << std::endl;
+	std::cout << "2.See the address \t:[" << r << "]" << std::endl;
+	r = dest; //destroy the object r  and point to different address
+	std::cout << "\n1.See the address \t:[" << dest << "]" << std::endl;
+	std::cout << "2.See the address \t:[" << r << "]" << std::endl;
+
+
+
+
+
+	return 0;
+}
---
+++ Dynamic Memory/Dynamic Memory.h
@@ -0,0 +1,37 @@
+﻿// Dynamic Memory.h : Include file for standard system include files,
+// or project specific include files.
+
+#pragma once
+
+#include <iostream>
+#include <memory>
+#include <list>	
+#include <vector>
+
+std::shared_ptr<int> factory(int a) {
+	
+	return std::make_shared<int>();
+}
+
+class strBlob {
+public:
+	typedef std::vector<std::string>::size_type size_type;
+	strBlob();
+	strBlob(std::initializer_list<std::string> il);
+	size_type size() const { return data->size(); }
+	bool empty() const { return data->empty(); }
+	void push_back(const std::string& t) {
+		data->push_back(t);
+
+	}
+	void  pop_back();
+	std::string& front();
+	std::string& back();
+
+private:
+	std::shared_ptr<std::vector<std::string>> data;
+	void check(size_type i, const std::string& msg) const;
+
+};
+
+// TODO: Reference additional headers your program requires here.
